069aba8c0a30cc70a2cd460dcdb79dde4296180c
Arthur <takerarthur@gmail.com>
2020-05-14 17:54:06 +0300

    Initial Commit

---
+++ webViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  webViewDemo
+//
+//  Created by Arthur Batyaev on 5/14/20.
+//  Copyright © 2020 Arthur Batyaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
